5f7514d51e4544134207b436654810f29e593504
Jm0nti <jmontielv@unal.edu.co>
2023-06-17 10:14:15 -0500

    ejercicio1

---
+++ Ejercicio1/Principal.java
@@ -0,0 +1,10 @@
+package Ejercicio1;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        VentanaPrincipal miVentanaPrincipal;
+        miVentanaPrincipal= new VentanaPrincipal();
+        miVentanaPrincipal.setVisible(true);
+    }
+}
